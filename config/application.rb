0203a8e48ec12d3703158280caa665cdbe021994
Kirk Jarvis <zuuzlo@yahoo.com>
2016-11-19 22:24:48 -0600

    changed assets compile2

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,7 @@ module Ideas
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    #added 11-19-2016
+    config.assets.prefix = 'prepackaged-assets'
   end
 end
