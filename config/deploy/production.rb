b7df20faa209a36049b1af4544d441bb277a0500
Kirk Jarvis <zuuzlo@yahoo.com>
2016-11-27 20:12:25 -0600

    sidekiq start auto1

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -15,6 +15,8 @@ set :deploy_to, "/home/#{fetch(:deploy_user)}/apps/#{fetch(:full_app_name)}"
 
 # dont try and infer something as important as environment from
 # stage name.
+
+
 set :rails_env, :production
 
 # number of unicorn workers, this will be reflected in
@@ -24,3 +26,5 @@ set :unicorn_worker_count, 1
 # whether we're using ssl or not, used for building nginx
 # config file
 set :enable_ssl, false
+
+set :sidekiq_config, "#{fetch(:deploy_to)}/current/config/sidekiq.yml"
