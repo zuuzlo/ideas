7a0bd883d4d454bd5c0956d9cdb41fecf95da0ba
Kirk Jarvis <zuuzlo@yahoo.com>
2016-12-09 21:11:11 -0600

    sidekiq cron badger mail

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,4 +4,10 @@ end
 
 Sidekiq.configure_client do |config|
     config.redis = { url: 'redis://localhost:6379/0', namespace: "sidekiq_ideas_#{Rails.env}" }
-end  
+end
+
+schedule_file = "config/schedule.yml"
+
+if File.exists?(schedule_file) && Sidekiq.server?
+  Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
+end
