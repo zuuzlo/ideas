11e17d5778718e85da0045fa8295eab5461b7708
Kirk Jarvis <zuuzlo@yahoo.com>
2016-12-09 21:28:35 -0600

    sidekiq cron badger mail

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -6,6 +6,8 @@ Sidekiq.configure_client do |config|
     config.redis = { url: 'redis://localhost:6379/0', namespace: "sidekiq_ideas_#{Rails.env}" }
 end
 
+Sidekiq::Web.set :session_secret, Rails.application.secrets.secret_key_base
+
 schedule_file = "config/schedule.yml"
 
 if File.exists?(schedule_file) && Sidekiq.server?
